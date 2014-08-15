98d142da651bdde1f06fee8dbd349a057129253c
Harmony Lee <lthung1504@gmail.com>
2014-08-15 16:39:29 +0700

    use base activity

---
+++ mobile/src/main/java/vn/siliconstraits/nicestop/activities/MainActivity.java
@@ -0,0 +1,24 @@
+package vn.siliconstraits.nicestop.activities;
+
+import vn.siliconstraits.nicestop.R;
+import vn.siliconstraits.nicestop.base.BaseActivity;
+
+/**
+ * Created by HarmonyLee on 8/15/14.
+ */
+public class MainActivity extends BaseActivity {
+    @Override
+    protected int getLayoutResourceId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void setUpControl() {
+
+    }
+
+    @Override
+    protected void setUpActionForControl() {
+
+    }
+}
