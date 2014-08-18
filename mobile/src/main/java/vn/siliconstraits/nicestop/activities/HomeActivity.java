413eef83fcdaddff8a6f4bc00d5742a5b0860282
Harmony Lee <lthung1504@gmail.com>
2014-08-18 17:57:44 +0700

    add HomeActivity

---
+++ mobile/src/main/java/vn/siliconstraits/nicestop/activities/HomeActivity.java
@@ -0,0 +1,24 @@
+package vn.siliconstraits.nicestop.activities;
+
+import vn.siliconstraits.nicestop.R;
+import vn.siliconstraits.nicestop.base.BaseActivity;
+
+/**
+ * Created by HarmonyLee on 8/18/14.
+ */
+public class HomeActivity extends BaseActivity {
+    @Override
+    protected int getLayoutResourceId() {
+        return R.layout.activity_home;
+    }
+
+    @Override
+    protected void setUpControl() {
+
+    }
+
+    @Override
+    protected void setUpActionForControl() {
+
+    }
+}
