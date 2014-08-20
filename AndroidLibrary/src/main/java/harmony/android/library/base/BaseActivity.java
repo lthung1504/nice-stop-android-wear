95e17c3dc713b77b6d1ce393f26b0a6034c90cba
Harmony Lee <lthung1504@gmail.com>
2014-08-20 17:31:57 +0700

    update BaseActivity

--- AndroidLibrary/src/main/java/harmony/android/library/base/BaseActivity.java
+++ AndroidLibrary/src/main/java/harmony/android/library/base/BaseActivity.java
@@ -2,7 +2,9 @@ package harmony.android.library.base;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.content.IntentCompat;
 
 import harmony.android.library.utils.LogManager;
 
@@ -50,4 +52,44 @@ public abstract class BaseActivity extends Activity {
     public Context getContext() {
         return this;
     }
+
+    public void goToActivity(Class objectClass, Bundle bundle) {
+        goToActivity(objectClass, false, bundle);
+    }
+
+    public void goToActivity(Class objectClass) {
+        goToActivity(objectClass, false, null);
+    }
+
+    public void goToActivity(Class objectClass, boolean isClearHistoryActivity, Bundle bundle) {
+        goToActivity(objectClass, isClearHistoryActivity, false, bundle);
+    }
+
+    public void goToActivity(Class objectClass, boolean isClearHistoryActivity) {
+        goToActivity(objectClass, isClearHistoryActivity, false, null);
+    }
+
+    public void goToActivity(Class objectClass, boolean isClearHistoryActivity, boolean isNoAnimation, Bundle bundle) {
+        LogManager.logI(TAG, "goToActivity " + objectClass);
+
+        Intent i = createIntent(objectClass, isClearHistoryActivity, isNoAnimation, bundle);
+
+        //		if (isClearHistoryActivity)
+        //			startActivity(i);
+        //		else
+        //			startActivityForResult(i, ACTIVITY_RESULT_CODE_ANIMATION);
+        startActivity(i);
+    }
+
+    private Intent createIntent(Class objectClass, boolean isClearHistoryActivity, boolean isNoAnimation, Bundle bundle) {
+        Intent i = new Intent(this, objectClass);
+        if (isClearHistoryActivity) {
+            LogManager.logD(TAG, "go to home activity set clear all task");
+            i.setFlags(IntentCompat.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        }
+        if (isNoAnimation) i.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        if (bundle != null) i.putExtras(bundle);
+        return i;
+    }
+
 }
